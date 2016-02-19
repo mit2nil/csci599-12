e8947c710d854cbae874a53d554982c826736063
mit2nil <mit2nil@gmail.com>
2016-02-18 21:40:39 -0800

    Added exception handling

--- HW1/PreProcessv2/App.java
+++ HW1/PreProcessv2/App.java
@@ -13,73 +13,97 @@ public class App
     private static File outputFolder;
     private static Tika tk = new Tika();
     
-    public static void main( String[] args ) throws IOException
+    public static void main( String[] args )
     {
-        String inputFolderPath = "C:\\stuff\\Git\\csci599-12\\HW1\\PreProcessv2\\in\\";
-        String outputFolderPath = "C:\\stuff\\Git\\csci599-12\\HW1\\PreProcessv2\\out\\";
-        inputFolder = new File(inputFolderPath);
-        outputFolder = new File(outputFolderPath);
-        
-        // Run Tika
-        detectTika(inputFolder);
-        
-        // Dump to Json
-        JsonWriter jsonWriter = null;
-        jsonWriter = new JsonWriter(new FileWriter(outputFolder.getAbsolutePath()+"\\mime_diversity.json"));
-        jsonWriter.setIndent("    ");
-        jsonWriter.beginObject();
-        jsonWriter.name("Mime_diversity");
-        jsonWriter.beginArray();
-        jsonWriter.beginObject();
-        Set<String> itr = mimetypes.keySet();
-        for(String s: itr)
+        try
         {
-            jsonWriter.name(s);
-            jsonWriter.value(mimetypes.get(s).toString());
+            String inputFolderPath = "C:\\stuff\\Git\\csci599-12\\HW1\\PreProcessv2\\in\\";
+            String outputFolderPath = "C:\\stuff\\Git\\csci599-12\\HW1\\PreProcessv2\\out\\";
+            inputFolder = new File(inputFolderPath);
+            outputFolder = new File(outputFolderPath);
+            
+            // Run Tika
+            detectTika(inputFolder);
+            
+            // Dump to Json
+            JsonWriter jsonWriter = null;
+            jsonWriter = new JsonWriter(new FileWriter(outputFolder.getAbsolutePath()+"\\mime_diversity.json"));
+            jsonWriter.setIndent("    ");
+            jsonWriter.beginObject();
+            jsonWriter.name("Mime_diversity");
+            jsonWriter.beginArray();
+            jsonWriter.beginObject();
+            Set<String> itr = mimetypes.keySet();
+            for(String s: itr)
+            {
+                jsonWriter.name(s);
+                jsonWriter.value(mimetypes.get(s).toString());
+            }
+            jsonWriter.endObject();
+            jsonWriter.endArray();            
+            jsonWriter.endObject();   
+            jsonWriter.close();
         }
-        jsonWriter.endObject();
-        jsonWriter.endArray();            
-        jsonWriter.endObject();   
-        jsonWriter.close();
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            System.out.println("IOException occured");
+        }
+        finally 
+        {
+            System.out.println("IOException occured");
+        }
     }
     
-    private static void detectTika(File dir) throws IOException
+    private static void detectTika(File dir)
     {
-        for (File f: dir.listFiles())
+        try
         {
-            //System.out.println("Traversing: "+f);
-            if(f.isDirectory())
+            for (File f: dir.listFiles())
             {
-                // Recursiely traverse
-                detectTika(f);
-            }
-            else
-            {
-                String contentType = tk.detect(f);
-                //System.out.println("Tika detected: "+contentType);
-                contentType = contentType.replace("/", "_");
-                
-                if(mimetypes.containsKey(contentType))
+                //System.out.println("Traversing: "+f);
+                if(f.isDirectory())
                 {
-                    mimetypes.put(contentType, (mimetypes.get(contentType)+1));
+                    // Recursiely traverse
+                    detectTika(f);
                 }
                 else
                 {
-                    mimetypes.put(contentType,1);
+                    String contentType = tk.detect(f);
+                    //System.out.println("Tika detected: "+contentType);
+                    contentType = contentType.replace("/", "_");
+                    
+                    if(mimetypes.containsKey(contentType))
+                    {
+                        mimetypes.put(contentType, (mimetypes.get(contentType)+1));
+                    }
+                    else
+                    {
+                        mimetypes.put(contentType,1);
+                    }
+                    
+                    // Create mime type directory 
+                    File output = new File(outputFolder.getAbsolutePath() +"\\" + contentType);
+                    if(!output.isDirectory())
+                    {
+                        output.mkdir();
+                    }
+                    
+                    // Move file to mime directory
+                    String fileName = f.getName();
+                    f.renameTo(new File(output.getAbsolutePath()+"\\" + fileName));
+                    //System.out.println("Moved "+f.getAbsolutePath()+" to "+output.getAbsolutePath()+"\\" + fileName);
                 }
-                
-                // Create mime type directory 
-                File output = new File(outputFolder.getAbsolutePath() +"\\" + contentType);
-                if(!output.isDirectory())
-                {
-                    output.mkdir();
-                }
-                
-                // Move file to mime directory
-                String fileName = f.getName();
-                f.renameTo(new File(output.getAbsolutePath()+"\\" + fileName));
-                //System.out.println("Moved "+f.getAbsolutePath()+" to "+output.getAbsolutePath()+"\\" + fileName);
             }
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            System.out.println("IOException occured");
+        }
+        finally 
+        {
+            System.out.println("IOException occured");
         }
     }
 }
