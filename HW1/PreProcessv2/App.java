d776a3c1afce4121d588caddbcf8ccf669ed324a
mit2nil <mit2nil@gmail.com>
2016-02-18 16:49:37 -0800

    Pre processing v2

---
+++ HW1/PreProcessv2/App.java
@@ -0,0 +1,85 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Set;
+import org.apache.tika.Tika;
+import com.google.gson.stream.JsonWriter;
+
+public class App 
+{
+    private static HashMap<String,Integer> mimetypes = new HashMap<String,Integer>();
+    private static File inputFolder;
+    private static File outputFolder;
+    
+    public static void main( String[] args ) throws IOException
+    {
+        String inputFolderPath = "C:\\stuff\\Git\\csci599-12\\HW1\\PreProcessv2\\in\\";
+        String outputFolderPath = "C:\\stuff\\Git\\csci599-12\\HW1\\PreProcessv2\\out\\";
+        inputFolder = new File(inputFolderPath);
+        outputFolder = new File(outputFolderPath);
+        
+        // Run Tika
+        detectTika(inputFolder);
+        
+        // Dump to Json
+        JsonWriter jsonWriter = null;
+        jsonWriter = new JsonWriter(new FileWriter(outputFolder.getAbsolutePath()+"\\mime_diversity.json"));
+        jsonWriter.setIndent("    ");
+        jsonWriter.beginObject();
+        jsonWriter.name("Mime_diversity");
+        jsonWriter.beginArray();
+        jsonWriter.beginObject();
+        Set<String> itr = mimetypes.keySet();
+        for(String s: itr)
+        {
+            jsonWriter.name(s);
+            jsonWriter.value(mimetypes.get(s).toString());
+        }
+        jsonWriter.endObject();
+        jsonWriter.endArray();            
+        jsonWriter.endObject();   
+        jsonWriter.close();
+    }
+    
+    private static void detectTika(File dir) throws IOException
+    {
+        Tika tk = new Tika();
+        for (File f: dir.listFiles())
+        {
+            //System.out.println("Traversing: "+f);
+            if(f.isDirectory())
+            {
+                // Recursiely traverse
+                detectTika(f);
+            }
+            else
+            {
+                String contentType = tk.detect(f);
+                //System.out.println("Tika detected: "+contentType);
+                contentType = contentType.replace("/", "_");
+                
+                if(mimetypes.containsKey(contentType))
+                {
+                    mimetypes.put(contentType, (mimetypes.get(contentType)+1));
+                }
+                else
+                {
+                    mimetypes.put(contentType,1);
+                }
+                
+                // Create mime type directory 
+                File output = new File(outputFolder.getAbsolutePath() +"\\" + contentType);
+                if(!output.isDirectory())
+                {
+                    output.mkdir();
+                }
+                
+                // Move file to mime directory
+                String fileName = f.getName();
+                f.renameTo(new File(output.getAbsolutePath()+"\\" + fileName));
+                //System.out.println("Moved "+f.getAbsolutePath()+" to "+output.getAbsolutePath()+"\\" + fileName);
+            }
+        }
+    }
+}
---
+++ HW1/PreProcessv2/readme.md
@@ -0,0 +1,14 @@
+# How-to
+
+*Useful references*
+  * [How to use external jar](http://www.programcreek.com/2014/01/compile-and-run-java-in-command-line-with-external-jars/)
+  * [Download Google Gson jar] (http://search.maven.org/#artifactdetails|com.google.code.gson|gson|2.6.1|jar)
+  * [Gson reference] (https://github.com/google/gson)
+
+*Compile and Run (Windows)*
+  * javac -cp ".;gson-2.6.1.jar;tika-app-1.11.jar" BFA.java
+  * java -cp ".;gson-2.6.1.jar;tika-app-1.11.jar" BFA
+
+*Compile and Run (Linux/Mac)*
+  * javac -cp ".:gson-2.6.1.jar;tika-app-1.11.jar" BFA.java
+  * java -cp ".:gson-2.6.1.jar;tika-app-1.11.jar" BFA
