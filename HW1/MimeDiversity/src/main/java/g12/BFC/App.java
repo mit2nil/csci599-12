2d260eb63b5b7b7a3c6c22f3311ebbed9f88e6f2
Kinara Shah <kinarahs@usc.edu>
2016-03-01 21:04:21 -0800

    Updated changes.

--- HW1/MimeDiversity/src/main/java/g12/BFC/App.java
+++ HW1/MimeDiversity/src/main/java/g12/BFC/App.java
@@ -1,6 +1,4 @@
 package g12.BFC;
-import java.io.IOException;
-
 import g12.BFC.BFA;
 
 /**
@@ -16,32 +14,24 @@ import g12.BFC.BFA100;
 
 public class App 
 {
-	public static void main(String[] args) throws IOException{
-		
-		//contains the root, stores the mime_type directories and hosts the fingerprint files
-	
-		String mainDirectory = args[0];
-		String mimeType = args[1];
+	public static void main(String[] args){
 		//Give input path of the mime type. 
-//		BFA bfa = new BFA(mainDirectory+"/"+mimeType);
-//		//Check if BFA succeeds. 
-//		boolean status = bfa.computeBFA(true);
-//		if(!status){
-//			System.out.println("Byte Frequency Analysis Failed");
-//			return;
-//		}
-		
+		BFA bfa = new BFA("/Users/kshah/output/audio_x-ms-wma");
+		//Check if BFA succeeds. 
+		boolean status = bfa.computeBFA(true);
+		if(!status){
+			System.out.println("Byte Frequency Analysis Failed");
+			return;
+		}
 		//Give input of the format : path to dir of 25% files, path of the Main folder[to search for BFA_mimeType.json]. 
 		//Below code will calculate both BFA for 100% files and BFC for 25% files. 
-		BFA100 bfa100 = new BFA100(mainDirectory + "/" + mimeType + "/" + mimeType + "_25",mainDirectory);
+		BFA100 bfa100 = new BFA100("/Users/kshah/output/audio_x-ms-wma","/Users/kshah/output/");
 		
 		//Give input of the format : Main folder[to search for BFA100_mimeType.json],Test Directory path. 
 		//Test directory could contain non-classified files or files you want to see correlation matrix against. 
 		//Either way out, it will output a matrix of the correlation between the file and all mime types that exist. 
-		BFCC bfcc = new BFCC(mainDirectory,"/Users/kshah/output/B2/test");
+		BFCC bfcc = new BFCC("/Users/kshah/output/","/Users/kshah/output/audio_x-ms-wma");
 		
-		FHT f = new FHT(mainDirectory + "/" + mimeType + "/" + mimeType + "_75", "DIRECTORY");
-		//f.detectFileScore(mainDirectory, new String("path to test files/file"), new String("mimetype or unknown"));
 		
 		
 	}
