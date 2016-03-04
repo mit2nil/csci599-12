3764a1b7ed066e85ddc7a36231ce90016f3f362d
mit2nil <mit2nil@gmail.com>
2016-03-04 01:09:02 -0800

    Extra credit 9 done

--- HW1/extra-credit/9/contentBasedDetection.java
+++ HW1/extra-credit/9/contentBasedDetection.java
@@ -1,100 +1,90 @@
 import org.apache.tika.Tika;
+import org.apache.tika.detect.NNExampleModelDetector;
+import org.apache.tika.io.TikaInputStream;
+import org.apache.tika.metadata.Metadata;
+import java.io.*;
 
 public class contentBasedDetection 
 {
-    private static Tika tk = new Tika();
-    
     public static void main( String[] args )
     {
+        Tika tk1 = new Tika();
+        NNExampleModelDetector tk2 = new NNExampleModelDetector();    // Content based detector
         try
         {
-            String inputFolderPath = "C:\\stuff\\Git\\csci599-12\\HW1\\PreProcessv2\\in\\";
-            String outputFolderPath = "C:\\stuff\\Git\\csci599-12\\HW1\\PreProcessv2\\out\\";
-            inputFolder = new File(inputFolderPath);
-            outputFolder = new File(outputFolderPath);
-            
-            // Run Tika
-            detectTika(inputFolder);
-            
-            // Dump to Json
-            JsonWriter jsonWriter = null;
-            jsonWriter = new JsonWriter(new FileWriter(outputFolder.getAbsolutePath()+"\\mime_diversity.json"));
-            jsonWriter.setIndent("    ");
-            jsonWriter.beginObject();
-            jsonWriter.name("Mime_diversity");
-            jsonWriter.beginArray();
-            jsonWriter.beginObject();
-            Set<String> itr = mimetypes.keySet();
-            for(String s: itr)
+            String dirpath = "C:\\stuff\\Git\\csci599-12\\HW1\\extra-credit\\9\\";
+            String mime = "data\\sample";
+            String modelName = "tika.model";
+          
+            File dir = new File(dirpath+mime);
+            for (File f: dir.listFiles())
             {
-                jsonWriter.name(s);
-                jsonWriter.value(mimetypes.get(s).toString());
+                //System.out.println("Traversing: "+f);
+                if(!f.isDirectory())
+                {
+                    String contentType = tk1.detect(f);
+                    System.out.println("Tika detected with default detector: "+contentType);
+                    
+                    tk2.loadDefaultModels(new File(dirpath+modelName));
+                    Metadata m = new Metadata();
+                    contentType = tk2.detect(TikaInputStream.get(f, m),m).toString();
+                    System.out.println("Tika detected with content based detector: "+contentType);
+                }
             }
-            jsonWriter.endObject();
-            jsonWriter.endArray();            
-            jsonWriter.endObject();   
-            jsonWriter.close();
         }
         catch (IOException e)
         {
             e.printStackTrace();
-            System.out.println("IOException occured");
-        }
-        finally 
-        {
             System.out.println("IOException occured");
         }
     }
     
-    private static void detectTika(File dir)
+    private static float[] getBFA(File f)
     {
-        try
+        float[] signatures = new float[256];
+        FileInputStream in = null;
+        
+        // Count freq.
+        try 
         {
-            for (File f: dir.listFiles())
-            {
-                //System.out.println("Traversing: "+f);
-                if(f.isDirectory())
-                {
-                    // Recursiely traverse
-                    detectTika(f);
-                }
-                else
-                {
-                    String contentType = tk.detect(f);
-                    //System.out.println("Tika detected: "+contentType);
-                    contentType = contentType.replace("/", "_");
-                    
-                    if(mimetypes.containsKey(contentType))
-                    {
-                        mimetypes.put(contentType, (mimetypes.get(contentType)+1));
-                    }
-                    else
-                    {
-                        mimetypes.put(contentType,1);
-                    }
-                    
-                    // Create mime type directory 
-                    File output = new File(outputFolder.getAbsolutePath() +"\\" + contentType);
-                    if(!output.isDirectory())
-                    {
-                        output.mkdir();
-                    }
-                    
-                    // Move file to mime directory
-                    String fileName = f.getName();
-                    f.renameTo(new File(output.getAbsolutePath()+"\\" + fileName));
-                    //System.out.println("Moved "+f.getAbsolutePath()+" to "+output.getAbsolutePath()+"\\" + fileName);
-                }
-            }
+            in = new FileInputStream(f);
+            int b;
+            while ((b = in.read()) != -1)
+                signatures[b] += 1;
         }
         catch (IOException e)
         {
             e.printStackTrace();
-            System.out.println("IOException occured");
+            System.out.println("IOException reading file: "+f.getName());
         }
         finally 
         {
-            System.out.println("IOException occured");
+            try
+            {
+                if (in != null)
+                    in.close();
+            }
+            catch(IOException e)
+            {
+                e.printStackTrace();
+                System.out.println("IOException closing file: "+f.getName());
+            }
         }
+        
+        // normalize it
+        float max = 0;
+        
+        // Find max
+        for (int i=0;i<256;i++)
+        {
+            if (max < signatures[i])
+                max = signatures[i];
+        }
+        
+        // Normalize and write as a string
+        for (int i=0;i<256;i++)
+            signatures[i] = (float) signatures[i]/max;
+        
+        return signatures;
     }
 }
